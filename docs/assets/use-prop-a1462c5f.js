aaeb7ae8358d346b8ea85e9e57d12c9e872d65a1
UpperCod <mtrujillo@outlook.cl>
2020-08-16 16:38:04 -0400

    minify build

--- docs/assets/use-prop-a1462c5f.js
+++ docs/assets/use-prop-a1462c5f.js
@@ -1,14 +1,1 @@
-import { u as useHost } from './custom-element-6d69df93.js';
-
-function useProp(name) {
-    let ref = useHost();
-    if (name in ref.current) {
-        if (!ref[name]) {
-            ref[name] = [null, (nextValue) => (ref.current[name] = nextValue)];
-        }
-        ref[name][0] = ref.current[name];
-        return ref[name];
-    }
-}
-
-export { useProp as u };
+import{u as r}from"./custom-element-6d69df93.js";function t(t){let e=r();if(t in e.current)return e[t]||(e[t]=[null,r=>e.current[t]=r]),e[t][0]=e.current[t],e[t]}export{t as u};
